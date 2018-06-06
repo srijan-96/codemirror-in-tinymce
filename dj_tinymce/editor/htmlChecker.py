95e64eda8c258567d204dcb97a37df0a3add404a
Ashutosh Sathe <2ashutoshbs@gmail.com>
2018-06-06 13:36:49 +0530

    Try our iframe solution[TEST]
    It doesn't work properly for now,
    there's problem in replacing the htmlContent using django
    In our iframe solution, we also have a javascript running along with it
    So, when django places it in html, it is not getting parsed properly
    
    If you want a stable working code, just skip the commit with tag [TEST]

--- dj_tinymce/editor/htmlChecker.py
+++ dj_tinymce/editor/htmlChecker.py
@@ -2,9 +2,30 @@
 # Will check if the style tag is empty 
 # if it is empty, return as it is
 # otherwise wrap it in our custom iframe
+template = '''<iframe id="mySpecialIframe" width="100%" height="0px" frameborder="0" style="margin: 0px !important; min-height: 5px;"></iframe>
+	<script type="text/javascript">
+		function writeInMyIFrame() {
+			var htmlString = String.raw`$$$`;
+			var myIframe = document.getElementById("mySpecialIframe").contentWindow;
+			myIframe.document.open();
+			myIframe.document.write(htmlString);
+			myIframe.document.close();
+		}
+		window.addEventListener('DOMContentLoaded', function doStuff(e) {
+			var myIframe = document.getElementById("mySpecialIframe");
+			myIframe.addEventListener("load", function(e) {
+				myIframe.height = myIframe.contentWindow.document.body.scrollHeight;
+				alert(myIframe.height);
+			});
+			document.close();
+		})
+		window.onload = writeInMyIFrame;
+	</script>
+    '''
 
 def process_for_iframe(htmlContent):
     ret = htmlContent
+    print(template)
     try:
         start = ret.index("<head>") + len("<head>")
         end = ret.index("</head>") + len("</head>")
@@ -17,4 +38,6 @@ def process_for_iframe(htmlContent):
         print("You don't need an iframe")
     else:
         print("You need an iframe")
+        parts = template.split('$$$')
+        ret = parts[0] + htmlContent + parts[1]
     return ret
